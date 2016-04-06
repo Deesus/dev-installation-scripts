613386c8a02a881bd55fac91acaffc800e264c93
Dee Reddy <d.reddy@yandex.com>
2016-04-06 04:46:14 -0400

    init commit

---
+++ vagrant_box_setup_ubuntu15.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script is a fix/workaround for Ubuntu 15.x
+# - Enables adding vagrant box
+# - Adds "precise64" box
+# this script is for you if you recieved an error like this when attempting `Vagrant up`:
+# "error setting certificate verify locations: CAfile: /etc/pki/tls/certs/ca-bundle.crt CApath: none"
+
+sudo apt-get -y install ca-certificates
+sudo mkdir /etc/pki/tls && sudo mkdir /etc/pki/tls/certs
+sudo cp /etc/ssl/certs/ca-certificates.crt /etc/pki/tls/certs/ca-bundle.crt
+sudo chmod 755 /etc/pki/tls/certs
+vagrant init precise64 http://files.vagrantup.com/precise64.box
+echo "precise64 box added!"
