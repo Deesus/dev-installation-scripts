03bf3070f81e94f272896a8b1382dc8ba6a03571
Dee Reddy <d.reddy@yandex.com>
2016-04-23 01:02:49 -0400

    bug: Fix option flags.

--- install_sass.sh
+++ install_sass.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 # Installs Sass/Compass for Ubuntu 15.10
 echo "Updating packages..."
-sudo apt-get update -yqq
+sudo apt-get -yqq update
 
 # N.b. Ruby and Rubygems should already be preinstalled in recent versions of Ubuntu
 echo "Updating Ruby/Rubygems..."
-sudo apt-get install --only-upgrade ruby -yqq
-sudo apt-get install --only-upgrade rubygems -yqq
+sudo apt-get -yqq install --only-upgrade ruby
+sudo apt-get -yqq install --only-upgrade rubygems
 
 # Install Sass:
 echo "Installing Sass..."
-sudo gem install sass -yqq
+sudo gem install sass -q
 
 # Install Compass:
 echo "Installing Compass..."
-sudo apt-get install gcc -yqq
-sudo gem install ffi -yqq
-sudo gem install compass -yqq
-sudo gem update -yqq
+sudo apt-get -yqq install gcc
+sudo gem install ffi -q
+sudo gem install compass -q
+sudo gem update -q
