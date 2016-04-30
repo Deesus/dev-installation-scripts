6122564cb4f21832a324951c9e46ba9ffc29f9a0
Dee Reddy <d.reddy@yandex.com>
2016-04-30 13:21:34 -0400

    update: Add packages.

--- install_dev_tools.sh
+++ install_dev_tools.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Note: assumes npm is installed
 # install Grunt cli:
-npm install -g grunt-cli
+sudo npm install -g grunt-cli
 
+# install GraphicsMagick:
+# GraphicsMagick works well with grunt-responsive-images or Imager.js
+sudo apt-get -qqy graphicsmagick
--- install_nodejs.sh
+++ install_nodejs.sh
@@ -5,8 +5,10 @@ wget https://npmjs.org/install.sh
 chmod +x install.sh
 sudo ./install.sh
 
-
 # Solution by David Walsh:
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
+
+# Install Express.js:
+sudo npm install -g express-generator
