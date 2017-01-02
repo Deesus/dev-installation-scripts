f270e70e1efc64a5b5e8531c925bfec0512af421
Dee Reddy <d.reddy@yandex.com>
2017-01-01 20:03:51 -0500

    Refactor: Consolidate and update dev script.

--- install_dev_tools.sh
+++ install_dev_tools.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 # Note: assumes npm is installed
-# install Grunt cli:
+
+# Install Grunt cli:
 sudo npm install -g grunt-cli
 
-# install GraphicsMagick:
+# Install Gulp cli:
+sudo npm install -g gulp-cli
+
+# Install GraphicsMagick:
 # GraphicsMagick works well with grunt-responsive-images or Imager.js
 sudo apt-get install -qqy graphicsmagick
+
+# Installs Sass/Compass for Ubuntu 15.10
+echo -e "\nUpdating packages..."
+sudo apt-get update
+
+# N.b. Ruby and Rubygems should already be preinstalled in recent versions of Ubuntu
+echo -e "\nUpdating Ruby/Rubygems..."
+sudo apt-get -y install --only-upgrade ruby
+sudo apt-get -y install --only-upgrade rubygems
+echo -e "\nInstalling ruby-dev..."
+sudo apt-get install ruby-dev -y
+
+# Install Sass:
+echo -e "\nInstalling Sass..."
+sudo gem install sass
+
+# Install Compass:
+echo -e "\nInstalling Compass..."
+sudo apt-get -yqq install gcc
+sudo gem install ffi
+sudo gem install compass
+
+# Update installed gems:
+echo -e "\nUpdating gems..."
+sudo gem update
+
+# Install Open JDK 8:
+echo -e "\nInstalling Open JDK 8..."
+sudo apt-get install openjdk-8-jdk -y
+
