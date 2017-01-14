2edae3e1936f9a26ecd8fd4788ff620eb078b65d
Deesus <d.reddy@yandex.com>
2017-01-14 09:19:07 -0500

    Refactor: Add Python dev.

--- dev_tools.sh
+++ dev_tools.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 # Install NodeJS 7.x
 echo "\nInstalling NodeJS 7 and NPM..."
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install nodejs -yq
 
 # Install ExpressJS
 echo "\nInstalling ExpressJS..."
@@ -31,7 +31,7 @@ echo "\nUpdating Ruby/Rubygems..."
 sudo apt-get -y install --only-upgrade ruby
 sudo apt-get -y install --only-upgrade rubygems
 echo "\nInstalling ruby-dev..."
-sudo apt-get install ruby-dev -y
+sudo apt-get install ruby-dev -yq
 
 # Install Sass:
 echo "\nInstalling Sass..."
@@ -39,7 +39,7 @@ sudo gem install sass
 
 # Install Compass:
 echo "\nInstalling Compass..."
-sudo apt-get -yqq install gcc
+sudo apt-get install gcc -yqq 
 sudo gem install ffi
 sudo gem install compass
 
@@ -50,18 +50,23 @@ sudo gem update
 # Install GraphicsMagick:
 # GraphicsMagick works well with grunt-responsive-images or Imager.js
 echo "\nInstalling GraphicsMagick..."
-sudo apt-get install graphicsmagick -y
+sudo apt-get install graphicsmagick -yq
 
 ################# Java #################
 
 # Install Open JDK 8:
 echo "\nInstalling Open JDK 8..."
-sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install openjdk-8-jdk -yq
 
 ################# Python #################
 
+# Install Python Dev Packages:
+echo "\nInstalling Python Dev..."
+sudo apt-get install python-dev -yq	# for Python 2.7
+sudo apt-get install python3-dev -yq	# for Python 3.x
+
 # Install important Python packages:
-echo "Installing important Python packages...\n"
-sudo apt-get install python-pip -y
+echo "\nInstalling important Python packages...\n"
+sudo apt-get install python-pip -yq
 pip install --upgrade pip
 pip install venv
