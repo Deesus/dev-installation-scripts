1cd5d99df4e553235ef7b6d78e44edb515ffcc35
deesus <d.reddy@yandex.com>
2018-02-10 02:17:52 -0500

    Refactor: Update dev intall.
    Added NodeJS and npm.
    Replaced SASS with LESS.

--- dev_tools.sh
+++ dev_tools.sh
@@ -3,49 +3,17 @@
 echo "\nUpdating packages..."
 sudo apt-get update
 
-################# Node #################
-# Install Node, NPM, and front-end task runners
+################# NodeJS #################
 
-# Install NodeJS 7.x
-echo "\nInstalling NodeJS 7 and NPM..."
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install nodejs -yq
-
-# Install ExpressJS
-echo "\nInstalling ExpressJS..."
-sudo npm install -g express-generator
-
-# Install Grunt cli:
-echo "\nInstalling Grunt..."
-sudo npm install -g grunt-cli
-
-# Install Gulp cli:
-echo "\nInstalling Gulp..."
-sudo npm install -g gulp-cli
+# Install NodeJS and npm:
+echo "\nInstalling NodeJS and npm..."
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 ################# Front-end tools #################
-# Install Sass/Compass, GraphicsMagick
 
-# N.b. Ruby and Rubygems may already be preinstalled in modern Debian/Ubuntu versions:
-echo "\nUpdating Ruby/Rubygems..."
-sudo apt-get -y install --only-upgrade ruby
-sudo apt-get -y install --only-upgrade rubygems
-echo "\nInstalling ruby-dev..."
-sudo apt-get install ruby-dev -yq
-
-# Install Sass:
-echo "\nInstalling Sass..."
-sudo gem install sass
-
-# Install Compass:
-echo "\nInstalling Compass..."
-sudo apt-get install gcc -yqq 
-sudo gem install ffi
-sudo gem install compass
-
-# Update installed gems:
-echo "\nUpdating gems..."
-sudo gem update
+# Install LESS:
+sudo npm install -g less -y
 
 # Install GraphicsMagick:
 echo "\nInstalling GraphicsMagick..."
@@ -54,12 +22,15 @@ sudo apt-get install graphicsmagick -yq	# GraphicsMagick works well with grunt/g
 ################# Java #################
 
 # Install Open JDK 8:
-echo "\nInstalling Open JDK 8..."
-sudo apt-get install openjdk-8-jdk -yq
+#echo "\nInstalling Open JDK 8..."
+#sudo apt-get install openjdk-8-jdk -yq
 
 ################# Python #################
 
 # Install Python Dev Packages:
+echo "\nInstalling python-apt..."
+sudo apt-get install python-apt
+
 echo "\nInstalling Python Dev..."
 sudo apt-get install python-dev -yq	# for Python 2
 sudo apt-get install python3-dev -yq	# for Python 3
